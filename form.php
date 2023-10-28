12b8d06878019754b02fea70f67894f4ff1dc633
Jude <jrroberts39@gmail.com>
2023-10-27 20:54:21 -0400

    distinguished style

--- form.php
+++ form.php
@@ -1,10 +1,14 @@
 <html>
+<link rel="stylesheet" href="style.css">
+<div class = "container">
 <h1>Sign in with existing account</h1> <p>select currently based on first name</p>
 <p>New User? <a href = "createUser.php">Create Account</a></p>
 <form action = "formProcess.php" method="POST">
 <h2>Email:</h2><input name ="email" type = "text"/>
 <h2>Password:</h2><input name = "pass" type = "text"/>
-<button type = "submit" name = "submit">submit</button>
+<button type = "submit" name = "submit">submit</button><br><br>
+<a href = "index.html">Return Home</a>
+</div>
 </form>
 </html>
 
